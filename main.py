430b8d05b23b3eed87cc1ffc6e5dd356473c55ee
Amortized <rahulmanghwani@gmail.com>
2015-11-29 20:37:38 -0800

    adding updated model code

--- main.py
+++ main.py
@@ -3,58 +3,170 @@ import sys;
 import numpy as np;
 import warnings;
 import math;
-
-#Read the data 
-train = pd.read_csv("train.csv");
-test  = pd.read_csv("test.csv");
+import model;
+from sklearn import preprocessing;
+import xgboost as xgb;
 
 def unique(column):
-  '''
-     Generates a list of unique elements for a column
-  '''
-  tr   = np.unique(train[column]);
-  tr   = [x for x in tr if str(x) != 'nan'];
-  te   = np.unique(test[column]);
-  te   = [x for x in te if str(x) != 'nan'];
-  temp = tr + te;
-  temp = list(np.unique(np.asarray(temp)));
-  return temp;
+	'''
+	 Generates a list of unique elements for a column
+	'''
+	tr   = np.unique(train[column]);
+	tr   = [x for x in tr if str(x) != 'nan'];
+	te   = np.unique(test[column]);
+	te   = [x for x in te if str(x) != 'nan'];
+	temp = list(set(tr) & set(te));
+	return temp;
 
-dpt = unique("DepartmentDescription");
-fln = unique("FinelineNumber");
-upc = unique("Upc");
+def UpcToKeep(train):
+	upc   = train['Upc'].value_counts()
+	upc_k = upc.keys();
+	upc_v = upc.values;
+	upc_v = upc_v / (1.0 * np.sum(upc_v));
+	return set(np.asarray(upc_k)[upc_v > 0.0005]);
 
+
+   
+def genFeatures(visits, dpt, fln, upc, week, mode="train"):
+    visit_feature = dict();
+    for name,group in visits:
+	  if mode == "train":
+	    label   = np.unique(group['TripType'])[0];
+	  #Weekday feature
+	  weekday = np.unique(group['Weekday'])[0];
+	  weekend = 0;
+	  if weekday in ["Sunday", "Saturday"]:
+		weekend = 1;
+	  weekday_f = [0] * len(week);
+	  weekday_f[week.index(weekday)] = 1; 		
+	  #Scan count
+	  items_boughts = group[group['ScanCount'] > 0];
+	  items_sold    = group[group['ScanCount'] < 0];
+	  #Count of items bought/sold
+	  items_boughts_sum    = np.sum(items_boughts['ScanCount']);
+	  items_boughts_mean   = np.mean(items_boughts['ScanCount']);		
+	  items_boughts_median = np.median(items_boughts['ScanCount']);
+	  items_sold_sum       = np.sum(items_sold['ScanCount']);
+	  
+	  #Unique 
+	  unique_upc_count     = 0;
+	  unique_dpt_count     = 0;
+	  unique_fln_count     = 0;
+		
+	  #Department
+	  items_boughts_dpt_f  = [0] * (1 + len(dpt));
+	  if items_boughts.empty == False:
+		items_boughts_dpt    = items_boughts[[True if str(x) != 'nan' \
+					   else False for x in items_boughts.DepartmentDescription]];
+		if items_boughts_dpt.empty == False:
+		  unique_dpt_count = len(np.unique(items_boughts_dpt['DepartmentDescription']));
+		  for name, group in items_boughts_dpt.groupby('DepartmentDescription'):
+		  	if name in dpt:
+		  	  items_boughts_dpt_f[dpt.index(name)]  = np.sum(group['ScanCount']);
+			else:
+			  items_boughts_dpt_f[len(items_boughts_dpt_f) - 1] = np.sum(group['ScanCount']);
+	  #FinelineNumber
+	  items_boughts_fln_f  = [0] * (1 + len(fln));
+	  if items_boughts.empty == False:
+		items_boughts_fln    = items_boughts[[True if str(x) != 'nan' \
+			               else False for x in items_boughts.FinelineNumber]];
+		if items_boughts_fln.empty == False:
+		  unique_fln_count = len(np.unique(items_boughts_fln['FinelineNumber']));    
+		  for name, group in items_boughts_fln.groupby('FinelineNumber'):
+			if name in fln:
+			  items_boughts_fln_f[fln.index(name)]  = np.sum(group['ScanCount']);
+			else:
+			  items_boughts_fln_f[len(items_boughts_fln_f) - 1] = np.sum(group['ScanCount']);
+	  #Upc
+	  items_boughts_Upc_f  = [0] * (1 + len(upc));
+	  if items_boughts.empty == False:
+		items_boughts_Upc    = items_boughts[[True if str(x) != 'nan' \
+			               else False for x in items_boughts.Upc]];
+		if items_boughts_Upc.empty == False:
+		  unique_upc_count = len(np.unique(items_boughts_Upc['Upc']));
+		  for name, group in items_boughts_Upc.groupby('Upc'):
+			if name in upc:
+			  items_boughts_Upc_f[upc.index(name)]  = np.sum(group['ScanCount']);        
+			else:
+			  items_boughts_Upc_f[len(items_boughts_Upc_f) - 1] = np.sum(group['ScanCount']);
+	  
+	  
+	  features =  weekday_f + [weekend, items_boughts_sum, items_boughts_mean,\
+	              items_boughts_median, items_sold_sum, unique_upc_count, \
+	              unique_dpt_count, unique_fln_count]  + \
+	              items_boughts_dpt_f + \
+	              items_boughts_fln_f + \
+	              items_boughts_Upc_f;
+	              
+	  if mode == "train":            
+	    #Feature
+	    visit_feature[name] = [features, label];
+	  else:
+	    visit_feature[name] = features;
+    return  visit_feature; 
+  
+
+#Read the data 
+train = pd.read_csv("./data/train.csv");
+test  = pd.read_csv("./data/test.csv");
+  
+###############
+dpt      = unique("DepartmentDescription");
+fln      = unique("FinelineNumber");
+upc      = list(set(unique("Upc")) & UpcToKeep(train));
+week     = list(np.unique(train['Weekday']));
+tripType = np.sort(np.unique(train.TripType));
+le       = preprocessing.LabelEncoder();
+le.fit(tripType);
 
 #Prepare features at the visit level
-train_visits = train.groupby('VisitNumber');
+train        = train.groupby('VisitNumber');
+train        = genFeatures(train, dpt, fln, upc, week, "train")
 
-for name,group in train_visits:
-  label   = np.unique(group['TripType'])[0];
-  #Weekday feature
-  weekday = np.unique(group['Weekday'])[0];
-  #Scan count
-  items_boughts = group[group['ScanCount'] > 0];
-  items_sold    = group[group['ScanCount'] < 0];
-  #Count of items bought/sold
-  items_boughts_sum    = np.sum(items_boughts['ScanCount']);
-  items_boughts_mean   = np.mean(items_boughts['ScanCount']);
-  items_boughts_median = np.median(items_boughts['ScanCount']);
-  items_sold_sum       = np.sum(items_sold['ScanCount']);
-  #Department
-  items_boughts_dpt_f  = [0] * len(dpt);
-  if items_boughts.empty == False:
-    items_boughts_dpt    = items_boughts[[True if str(x) != 'nan' else False for x in items_boughts.DepartmentDescription]];
-    if items_boughts_dpt.empty == False:
-      for name, group in items_boughts_dpt.groupby('DepartmentDescription'):
-        items_boughts_dpt_f[dpt.index(name)]  = np.sum(group['ScanCount']);
-  #FinelineNumber
-  items_boughts_fln_f  = [0] * len(fln);
-  if items_boughts.empty == False:
-    items_boughts_fln    = items_boughts[[True if str(x) != 'nan' else False for x in items_boughts.FinelineNumber]];
-    if items_boughts_fln.empty == False:
-      for name, group in items_boughts_fln.groupby('FinelineNumber'):
-        items_boughts_fln_f[fln.index(name)]  = np.sum(group['ScanCount']);
-        
+train_X      = [];
+train_Y      = [];
+for k in train.keys():
+  train_Y.append(train[k][1]);
+  train_X.append(train[k][0]);
+
+del train;  
+train_X      = np.array(train_X);
+train_Y      = np.array(train_Y);
+
+#Ensure labels are between (0,num_class-1)
+train_Y      = le.transform(train_Y);
+
+#Train
+best_model,best_n_trees = model.do(train_X, train_Y);
+
+del train_X, train_Y;
+
+#Test
+test        = test.groupby('VisitNumber');
+test        = genFeatures(test, dpt, fln, upc, week, "test")
 
 
+test_X      = [];
+for k in test.keys():
+  test_X.append(test[k]);
 
+
+test_visitNo = test.keys();
+del test;
+
+test_X      = np.array(test_X);
+
+test_X      = xgb.DMatrix(test_X, missing=float('NaN'));
+y_hat       = best_model.predict(test_X,ntree_limit=best_n_trees)
+
+
+df          = pd.read_csv("./data/sample_submission.csv");
+columns     = df.columns;
+
+#columns     = columns[:len(columns)-1]
+test_df     = pd.DataFrame(columns=columns);
+
+for i in range(0, len(test_visitNo)):
+  test_df.loc[i] = [test_visitNo[i]] + list(y_hat[i]);
+
+test_df.to_csv('./data/submission.csv', index=False);
---
+++ model.py
@@ -0,0 +1,43 @@
+"""
+
+__author__ = 'amortized'
+"""
+
+from sklearn.cross_validation import train_test_split
+import xgboost as xgb
+import numpy as np;
+
+#Read Data
+def do(train_X, train_Y):
+  X_train, X_test, Y_train, Y_test = train_test_split(train_X, train_Y, test_size=0.10, random_state=42);
+
+  dtrain      = xgb.DMatrix( X_train, label=Y_train, missing=float('NaN'));
+  dvalidation = xgb.DMatrix( X_test, label=Y_test,missing=float('NaN'));
+
+
+  del X_train, X_test, Y_train, Y_test;
+
+  #Track metrics on the watchlist
+  watchlist = [ (dtrain,'train'), (dvalidation, 'validation') ];
+
+  #Params
+  param      = {'eval_metric' : 'mlogloss', 'objective' : 'multi:softprob', 'nthread' : 16, \
+	            'colsample_bytree' : 1.0, 'subsample' : 1.0, 'eta': 0.01,\
+	            'seed' : 42, 'num_class' : len(np.unique(train_Y)) };
+
+  num_round  = 10;
+
+  classifier = xgb.train(param,dtrain,num_round,evals=watchlist,early_stopping_rounds=100);
+
+  metric     = classifier.best_score;
+  itr        = classifier.best_iteration;
+  print("\n Metric : " + str(metric) + " for Params " + str(param) + " occurs at " + str(itr));
+
+  del dtrain, dvalidation;
+
+  return classifier, itr;
+
+
+
+
+
